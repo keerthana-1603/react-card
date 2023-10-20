a0d8ee2f550069d6b1efedb15a95fbe4f503567a
keerthana-1603 <keerthanakani27gmail.com>
2023-10-20 22:56:57 +0530

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css'
 function App() {
+  
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1 className='heading'>React Price Card</h1>
+    <div className='app'> 
+    <Pricecard/>
+    <Pricecard2/>
+    <Pricecard3/>
+    </div> 
     </div>
-  );
+  )
+}
+//props
+export default App
+
+function Pricecard(){
+  return(
+    <div className='card'>
+      <div className='card-head'>
+        <h5 className='cardTop'>FREE</h5>
+        <h1>$0/month</h1>
+        <p className='outline'>______________________________</p></div>
+        <div className='card-body'>
+          <p>√ Single User</p>
+          <p>√ 50GB Storage</p>
+          <p>√ Unlimited Public Project</p>
+          <p>√ Community Access</p>
+          <p className='dim'>× Unlimited Private projects</p>
+          <p className='dim'>× Dedicated Phone Supports</p>
+          <p className='dim'>× Free Subdomain</p>
+          <p className='dim'>× Monthly Status Reports</p>
+          <button className='btn'>BUTTON</button>
+          </div>
+    </div>
+  )
 }
 
-export default App;
+function Pricecard2(){
+  return(
+    <div className='card'>
+      <div className='card-head'>
+        <h5 className='cardTop'>PLUS</h5>
+        <h1>$9/month</h1>
+        <p className='outline'>______________________________</p></div>
+        <div className='card-body'>
+          <p>√ Single User</p>
+          <p>√ 50GB Storage</p>
+          <p>√ Unlimited Public Project</p>
+          <p>√ Community Access</p>
+          <p>√ Unlimited Private projects</p>
+          <p>√ Dedicated Phone Supports</p>
+          <p>√ Free Subdomain</p>
+          <p className='dim'>× Monthly Status Reports</p>
+          <button className='btn'>BUTTON</button>
+          </div>
+    </div>
+  )
+}
+
+function Pricecard3(){
+  return(
+    <div className='card'>
+      <div className='card-head'>
+        <h5 className='cardTop'>PRO</h5>
+        <h1>$49/month</h1>
+        <p className='outline'>______________________________</p></div>
+        <div className='card-body'>
+          <p>√ Single User</p>
+          <p>√ 50GB Storage</p>
+          <p>√ Unlimited Public Project</p>
+          <p>√ Community Access</p>
+          <p>√ Unlimited Private projects</p>
+          <p>√ Dedicated Phone Supports</p>
+          <p>√ Free Subdomain</p>
+          <p>√ Monthly Status Reports</p>
+          <button className='btn'>BUTTON</button>
+          </div>
+    </div>
+  )
+}
